087c70438f929cfd4e5c9f5789840cd85fcd7cc9
arnav770 <77731681+arnav770@users.noreply.github.com>
2021-08-18 12:14:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+status = "";
+objects = [];
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocosst", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img,gotResult);
+}
+
+function gotResult() { 
+if(error) {
+    console.log(error);
+}
+console.log(results);
+objects = results;
+}
+
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if(status!= "") {
+        for(i=0; i<objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status : objects detected";  
+            fill("blue");
+            noFill();
+            percent = floor(objects[i].confidence * 100);
+            stroke("black");
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        
+        }
+    }
+   
+}
